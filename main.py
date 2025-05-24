ad694d41ceec5ec837c1fd7374519bd182857204
Safal Pokharel <safalpokharel@safals-air.mynetworksettings.com>
2025-05-23 20:04:53 -0400

    Save current changes before cleaning up

---
+++ main.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+from google import genai
+from rich import print as rprint
+from rich.panel import Panel
+from rich.console import Console
+import os
+import subprocess
+from utils.git_utils import is_git_repo, has_staged_changes, get_git_diff
+
+load_dotenv()
+
+
+rprint("🎉 WELCOME TO AUTO COMMIT MESSAGE GENERATOR")
+
+def main():
+    if is_git_repo():
+        if has_staged_changes():
+            diff = get_git_diff()
+
+            with open('./prompts/commit.txt', 'r') as file:
+                print("FILE OPENEDE")
+                content = file.read()
+                final_prompt = content.replace('{git_diff}', diff)
+               
+
+                try:
+                    
+
+                    client = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+
+                    response = client.models.generate_content(
+                        model="gemini-2.0-flash",
+                        contents="Explain how AI works in a few words",
+                    )
+
+                    print(response.text)
+
+
+
+
+
+                    print("\n✅ Suggested Commit Message:\n")
+                    commit_msg = response.text
+                    print(Panel(commit_msg, style="green"))
+                    edit_msg = input("Do you want to edit the message? Type Y or N").strip().upper()
+                    if edit_msg == "Y":
+                        with open("edit_commit.txt", "w") as file:
+                            file.write(commit_msg)
+                        subprocess.run(["nano", "edit_commit.txt"])
+                        with open("edit_commit.txt", "r") as file:
+                            commit_msg = file.read().strip()
+
+                    subprocess.run(["git", "commit", "-m", commit_msg])
+                    os.remove("edit_commit.txt")
+
+                except Exception as e :
+                    print(f"API CALL ERROR!: {e}")
+                
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/git_utils.py
@@ -0,0 +1,27 @@
+import subprocess
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+def is_git_repo():
+    response = subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], capture_output=True, text=True)
+    if response.returncode == 0 and response.stdout.strip() == "true":
+        return True
+    else:
+        print('❌ Not a Git repository. Please navigate into a Git repo and try again.')
+        return False
+
+
+def has_staged_changes():
+    try:
+        result = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
+        if not result.stdout.strip():
+            print('⚠️ No staged changes found. Please stage your changes using: git add <file> and try again.')
+            return False
+        return True
+    except Exception as e:
+        print(e)
+
+
+def get_git_diff():
+    diff = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
+    return diff.stdout
